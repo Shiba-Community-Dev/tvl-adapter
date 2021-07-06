961e2ba31b905db9d61b6df3c5b79843c18faa30
squad <eaglebaxter@gmail.com>
2021-07-07 01:02:12 +0200

    shibarium draft

---
+++ chains/shibarium/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    /* Project Metadata */
+    name: "Shibarium_Swap",
+    token: "STEAK",
+    chain: 'shibarium',
+    category: "Defi",
+    start: Uint32Array,
+    /* required for fetching token balances */
+    tokenHolderMap: [
+      {
+        tokens: '0x0',   // Steak
+        holders: '0x0'   // Shi
+      },
+    ]
+  };
